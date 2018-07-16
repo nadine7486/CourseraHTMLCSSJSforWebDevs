9ed2ba156d2d5e2d056817eab81e014f6b062df1
nadine7486 <nadine7486@hotmail.com>
2018-07-16 14:05:21 +0300

    change

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -117,7 +117,9 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"short_name",chosenCategoryShortName);
+      var homeHtmlToInsertIntoMainPage = homeHtml;
+      homeHtmlToInsertIntoMainPage = 
+        insertProperty(homeHtmlToInsertIntoMainPage,"short_name",chosenCategoryShortName);
 
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
